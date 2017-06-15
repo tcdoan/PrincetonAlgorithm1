6986e32c64f5699c3f8b4645777873068d917034
TDOAN1H\Thanh <doant@outlook.com>
2017-06-14 23:34:52 -0700

    Implement the solver.

--- Puzzle/src/Solver.java
+++ Puzzle/src/Solver.java
@@ -1,4 +1,6 @@
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.ResizingArrayQueue;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Solver
@@ -7,28 +9,109 @@ public class Solver
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial)
     {
+        MinPQ<SearchNode> masterQueue = new MinPQ<>();
+        MinPQ<SearchNode> twinQueue = new MinPQ<>();
 
+        SearchNode firstSearchNode = new SearchNode(initial,0,null);
+        SearchNode firstTwinNode = new SearchNode(initial.twin(),0,null);
+
+        masterQueue.insert(firstSearchNode);
+        twinQueue.insert(firstTwinNode);
+
+        isSolvable = false;
+        while (masterQueue.size() > 0 && twinQueue.size() > 0)
+        {
+            SearchNode twinNode = twinQueue.delMin();
+            twinSolution.enqueue(twinNode.board);
+            if (twinNode.board.isGoal())
+            {
+                isSolvable = false;
+                break;
+            }
+            else
+            {
+                SearchNode current = masterQueue.delMin();
+                solution.enqueue(current.board);
+                if (current.board.isGoal())
+                {
+                    isSolvable = true;
+                    break;
+                }
+
+                for (Board twinNeighbor : twinNode.board.neighbors())
+                {
+                    if (!twinNeighbor.equals(twinNode.board))
+                    {
+                        twinQueue.insert(new SearchNode(twinNeighbor, solution.size(), twinNode));
+                    }
+                }
+
+                for (Board n : current.board.neighbors())
+                {
+                    if (!n.equals(current.board))
+                    {
+                        masterQueue.insert(new SearchNode(n, twinSolution.size(), twinNode));
+                    }
+                }
+            }
+        }
     }
 
     // is the initial board solvable?
     public boolean isSolvable()
     {
-        return false;
-
+        return isSolvable;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
     public int moves()
     {
-        return 0;
-
+        if (isSolvable)
+        {
+            return solution.size()-1;
+        }
+        return -1;
     }
 
     // sequence of boards in a shortest solution; null if unsolvable
     public Iterable<Board> solution()
     {
+        if (isSolvable())
+        {
+            return solution;
+        }
         return null;
     }
+
+    private boolean isSolvable;
+    private ResizingArrayQueue<Board> solution = new ResizingArrayQueue<>();
+    private ResizingArrayQueue<Board> twinSolution = new ResizingArrayQueue<>();
+
+    private class SearchNode implements Comparable<SearchNode>
+    {
+        private Board board;
+        private int numMovesToReach;
+        private SearchNode prev;
+
+        public SearchNode(Board board, int numMovesToReach, SearchNode prev)
+        {
+            this.board = board;
+            this.numMovesToReach = numMovesToReach;
+            this.prev = prev;
+        }
+
+        public int priority()
+        {
+            return numMovesToReach + this.board.manhattan();
+        }
+
+        @Override
+        public int compareTo(SearchNode other)
+        {
+            return priority() - other.priority();
+        }
+    }
+
     // solve a slider puzzle (given below)
     public static void main(String[] args)
     {
@@ -46,8 +129,11 @@ public class Solver
 
         // print solution to standard output
         if (!solver.isSolvable())
+        {
             StdOut.println("No solution possible");
-        else {
+        }
+        else
+        {
             StdOut.println("Minimum number of moves = " + solver.moves());
             for (Board board : solver.solution())
                 StdOut.println(board);
