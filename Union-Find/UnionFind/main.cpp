326642b012e020fe0af9f92afd82195aead42a3f
Thanh Doan <doant@outlook.com>
2019-07-08 23:42:45 -0700

    Test 2 implementations. Recursive path compression gives similar perf numberson uf.txt input as weighted and path compression implementation.

---
+++ Union-Find/UnionFind/main.cpp
@@ -0,0 +1,54 @@
+#include "uf.h"
+#include <assert.h>
+#include <fstream>
+#include <iostream>
+#include <ctime>
+
+// Compile cl.exe /GS- /O2 main.cpp 
+// Run main.exe yields 2.8 seconds on uf.exe input 
+int main(char** args)
+{
+    ifstream ifs("uf.txt", ifstream::in);
+	int N;
+	ifs >> N;
+
+    clock_t begin = clock();	
+    UnionFind uf(N);
+	while (!ifs.eof())
+	{
+		uint32_t x;
+		uint32_t y;
+		ifs >> x;
+		ifs >> y;
+
+        if (uf.Connected(x, y)) 
+        {
+            continue;
+        }
+        uf.Union(x,y);
+	}
+
+    std::cout << uf.Count() << " components" << endl;
+    clock_t endt = clock();
+    std::cout << ( endt -begin ) * 1000.0 / CLOCKS_PER_SEC << " ms." << std::endl;
+}
+
+// Test UnionFind via assertion...
+int test(char** args)
+{
+    UnionFind uf(10);
+    uf.Union(4, 3);
+    uf.Union(3, 8);
+    uf.Union(6, 5);
+    uf.Union(9, 4);
+    uf.Union(2, 1);
+    uf.Union(5, 0);
+    uf.Union(7, 2);
+    // uf.Print();
+    assert(uf.Connected(2, 4) == false);
+    assert(uf.Connected(1, 7));
+    assert(uf.Connected(1, 5) == false);
+    assert(uf.Connected(0, 5));
+    assert(uf.Connected(3, 9));
+    return 0;
+}
---
+++ Union-Find/UnionFind/uf.h
@@ -0,0 +1,66 @@
+#include <vector>
+using namespace  std;
+
+class UnionFind 
+{
+public:
+    UnionFind(int n): n{n}, count{n}
+    {
+        id = vector<int>(n);
+        // sz = vector<int>(n); // Weighted UF
+        for(int i = 0; i < n; i++) 
+        {
+            id[i] = i;
+            // sz[i] = 1;
+        }
+    }
+
+    int root(int p) 
+    {
+        // while(p != id[p])
+        // {
+        //     // path compression 
+        //     id[p] = id[id[p]];
+        //     p = id[p];
+        // }        
+        return id[p] = id[p] == p ? p : root(id[p]);
+    }
+
+    bool Connected(int p, int q) 
+    {
+        return root(p) == root(q);
+    }
+
+    void Union(int p, int q) 
+    {
+        int rp = root(p);
+        int rq = root(q);
+        if(rp == rq) { return; }
+        count--;
+        id[rq] = rp;
+        // Weighted union find...
+        // if (sz[rp] < sz[rq]) 
+        // {
+        //     id[rp] = rq;
+        //     sz[rq] += sz[rp]; 
+        //     return;
+        // }
+        // id[rq] = rp;
+        // sz[rp] += sz[rq];                
+    }
+
+    int Count() 
+    {
+        return count;
+    }
+    
+private:
+    // number of nodes.
+    int n;
+    
+    // number of components
+    int count;
+
+    vector<int> id;  
+    // vector<int> sz;
+};
